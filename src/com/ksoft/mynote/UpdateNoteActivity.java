e910f1f4179880457f96edf64a738d3481d07082
Basu-Kar <basudeva.kar@gmail.com>
2014-09-17 22:08:36 +0530

    updated for

--- src/com/ksoft/mynote/UpdateNoteActivity.java
+++ src/com/ksoft/mynote/UpdateNoteActivity.java
@@ -16,6 +16,7 @@ import android.widget.TextView;
 public class UpdateNoteActivity extends Activity implements View.OnClickListener{
 
 	private NoteData noteData;
+	private String editNoteSubject=null;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +30,7 @@ public class UpdateNoteActivity extends Activity implements View.OnClickListener
 		Intent intent = getIntent();
 		
 		subjText.setText(intent.getStringExtra("noteSubject"));
+		editNoteSubject = intent.getStringExtra("noteSubject");
 		detailText.setText(intent.getStringExtra("noteDetails"));
 		idTxt.setText(intent.getStringExtra("noteId"));
 		
@@ -64,16 +66,48 @@ public class UpdateNoteActivity extends Activity implements View.OnClickListener
 
 	@Override
 	public void onClick(View v) {
-		TextView subjText = (TextView) findViewById(R.id.update_note_title);
-		TextView detailText = (TextView) findViewById(R.id.update_note_details_id);
-		TextView idTxt = (TextView) findViewById(R.id.update_note_id);
-		Note note = new Note(Integer.parseInt(idTxt.getText().toString()),subjText.getText().toString(),detailText.getText().toString());
-		System.out.println("Note "+note.getId()+" "+note.getSubject()+" "+note.getNote());
-		noteData.updateNote(note);
-		Intent intent = new Intent(this, HomeActivity.class);
-		intent.putExtra("virified", true);
-		
-		startActivity(intent);
+		if(isValidEntry()){
+			TextView subjText = (TextView) findViewById(R.id.update_note_title);
+			TextView detailText = (TextView) findViewById(R.id.update_note_details_id);
+			TextView idTxt = (TextView) findViewById(R.id.update_note_id);
+			
+			Note note = new Note(Integer.parseInt(idTxt.getText().toString()),subjText.getText().toString(),detailText.getText().toString());
+			//System.out.println("Note "+note.getId()+" "+note.getSubject()+" "+note.getNote());
+			noteData.updateNote(note);
+			Intent intent = new Intent(this, HomeActivity.class);
+			intent.putExtra("virified", true);
+			
+			startActivity(intent);
+		}
 		
 	}
+	
+	private boolean isValidEntry(){
+		boolean valid=true;
+		TextView noteView = (TextView) findViewById(R.id.update_note_details_id);
+		
+		TextView noteSubjView = (TextView) findViewById(R.id.update_note_title);
+		
+		if("".equals(noteSubjView.getText().toString().trim()) ){
+			noteSubjView.setError("Blank Not Allowed");
+			valid = false;
+		}else if(noteSubjView.getText().toString().trim().length()<3){
+			noteSubjView.setError("Minimum 3 characters");
+			valid = false;
+		}else if("".equals(noteView.getText().toString().trim()) ){
+			noteView.setError("Blank Not Allowed");
+			valid = false;
+		}else if(noteView.getText().toString().trim().length()<5){
+			noteView.setError("Minimum 5 characters");
+			valid = false;
+		}else if(editNoteSubject!=null && !editNoteSubject.equalsIgnoreCase(noteSubjView.getText().toString().trim())&&
+				noteData.isDuplicateTitle(noteSubjView.getText().toString().trim())){
+			noteView.setError("Note title already exists");
+			valid = false;
+		}
+		
+		
+		return valid;
+	}
+	
 }
